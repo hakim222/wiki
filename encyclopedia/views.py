6ac7d0d6330b1b60941855ef78a88221938781aa
Hakim Azizan <hazizan22@gmail.com>
2020-07-11 15:45:20 +0800

    Implemented Entry Page e requirements

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
 
 from . import util
+import markdown2
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def title(request, title):
+    
+    title = util.get_entry(title)
+
+    if title is not None:
+        return render(request, "encyclopedia/title.html", {
+            "entry": markdown2.markdown(title),
+            "title": title
+        })
+    
+    else:
+        return render(request, "encyclopedia/error.html")
 
