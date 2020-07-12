e50a8883600de0ac9523ba72b9bbab2a8e568470
Hakim Azizan <hazizan22@gmail.com>
2020-07-12 08:50:22 +0800

    Implemented Search, allow user to search entries in sidebar

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -7,11 +7,14 @@ import markdown2
 
 
 class NewTasksForm(forms.Form):
-    form = forms.CharField(label="New Task")
+    form = forms.CharField(label="search form", widget=forms.TextInput(attrs={'class':'search', 'placeholder':'Search Encyclopedia'}))
+
+search_form = NewTasksForm()
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
+        "search_form": search_form
     })
 
 def title(request, title):
@@ -21,16 +24,32 @@ def title(request, title):
     if title is not None:
         return render(request, "encyclopedia/title.html", {
             "entry": markdown2.markdown(title),
-            "title": title
+            "title": title,
+            "search_form": search_form
         })
     
     else:
         return render(request, "encyclopedia/error.html")
 
-# def search(request):
-#     if request.method == "POST":
-#         search = NewTasksForm(request.POST)
-#         if search.is_valid():
-#             search = search.cleaned_data["form"]
-#             return HttpResponseRedirect(reverse("title", [search]))
-  
+def search(request):
+    if request.method == "POST":
+        entries = util.list_entries()
+        searched_form = NewTasksForm(request.POST)
+        if searched_form.is_valid():
+            search = searched_form.cleaned_data["form"]
+            if search in entries:
+                return HttpResponseRedirect(reverse("title", kwargs={"title":search}))
+            else:
+                results = []
+                for entry in entries:
+                    if search in entry:
+                        results.append(entry)
+                if len(results) > 0:
+                    return render(request, "encyclopedia/search.html", {
+                        "entries": results,
+                        "search_form": search_form
+                    })            
+                else:
+                    return render(request, "encyclopedia/error.html", {
+                        "search_form": search_form
+                    })
